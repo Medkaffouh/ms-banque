94b7e7a2d718ce44c9155729373ae57f2b735ae5
Medkaffouh <kaffouhmohamed@gmail.com>
2022-06-21 17:20:25 +0100

    Web Services SOAP et REST avec Spring Boot en utitisant RestController (faut ajouter une dependence)

---
+++ src/main/java/com/med/msbanque/web/CompteRestControllerAPI.java
@@ -0,0 +1,44 @@
+package com.med.msbanque.web;
+
+import com.med.msbanque.entities.Compte;
+import com.med.msbanque.repositories.CompteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/banque")
+public class CompteRestControllerAPI {
+    @Autowired
+    private CompteRepository compteRepository;
+
+    @GetMapping("/comptes")
+    public List<Compte> compteList(){
+        return compteRepository.findAll();
+    }
+
+    @GetMapping("/comptes/{id}")
+    public Compte getCompte(@PathVariable Long id){
+        return compteRepository.findById(id).get();
+    }
+
+    @PostMapping(path = "/comptes")
+    public Compte save(@RequestBody Compte compte){
+        return compteRepository.save(compte);
+    }
+
+    @PutMapping("/comptes/{id}")
+    public Compte update(@RequestBody Compte compte, @PathVariable Long id){
+        compte.setId(id);
+        return compteRepository.save(compte);
+    }
+
+    @DeleteMapping("/comptes/{id}")
+    public void delete(@PathVariable Long id){
+        compteRepository.deleteById(id);
+    }
+}
--- src/main/java/com/med/msbanque/web/CompteRestJaxRSAPI.java
+++ src/main/java/com/med/msbanque/web/CompteRestJaxRSAPI.java
@@ -12,27 +12,29 @@ import java.util.List;
 @Component
 @Path("/banque")
 public class CompteRestJaxRSAPI {
-    @Autowired //-> l'injection de dependence ne peux faire que sur les composant spring, c-a-d spring ne va pas
+    //-> l'injection de dependence ne peux faire que sur les composant spring, c-a-d spring ne va pas
     // interpreter @Autowired si c'est ne pas spring qui a instancier la class
     // d'habitude on utilise @RestController (c'est un composant spring) mais @Path c'est pas un composant spring (pas un
     // annotation de spring il a connait pas)
     // c'est pour cela il foudrais ajouter l'annotation @Component
     // alors le fait d'ajouter l'annotation @Component c-a-d c'est un composant spring (spring qui va instancier, sinon tu va
     // avoir null pointer exception).
+    @Autowired
     private CompteRepository compteRepository;
 
-    @Path("/comptes")
-    @GET
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+
     // pour json jersey il utilise Jackson
     // pour xml jersey il utilise JaxB (il y'a un condition vous devez ajouter l'annotation @XmlRoutElement dans la class que vous allez serializer Compte (POJO)
+    @Path("/comptes")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
     public List<Compte> compteList(){
         return compteRepository.findAll();
     }
 
     @Path("/comptes/{id}")
     @GET
-    @Produces({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_XML})
     public Compte getCompte(@PathParam(value = "id") Long id){
         return compteRepository.findById(id).get();
     }
