ea7ae17ba2e36a4fa93470bde365fdd55e6e6f59
Medkaffouh <kaffouhmohamed@gmail.com>
2022-06-21 19:57:51 +0100

    comment crée un methode et utiliser dans le url et on termine la partie Rest avec les 3 technique (Jersey, RestController, Spring Data Rest)

--- src/main/java/com/med/msbanque/repositories/CompteRepository.java
+++ src/main/java/com/med/msbanque/repositories/CompteRepository.java
@@ -1,9 +1,18 @@
 package com.med.msbanque.repositories;
 
 import com.med.msbanque.entities.Compte;
+import com.med.msbanque.entities.TypeCompte;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface CompteRepository extends JpaRepository<Compte,Long> {
+    @RestResource(path = "/byType")
+    public List<Compte> findByType(@Param("t") TypeCompte type);
+    // Sans Annotation => /api/comptes/search/findByType?type=EPARGNE
+    // Avec Annotation => /api/comptes/search/byType?t=EPARGNE
 }
